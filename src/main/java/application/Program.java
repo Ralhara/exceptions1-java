eba33388e6370b1d02fb313f1e4e322cf4c24ad1
Raphael Magliano <maglianorq@gmail.com>
2023-12-16 03:40:20 -0300

    Bad solution

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -19,6 +19,7 @@ public class Program {
         System.out.print("Check-in date (dd/MM/yyyy): ");
         Date checkIn = sdf.parse(sc.next());
         System.out.print("Check-out date (dd/MM/yyyy): ");
+
         Date checkOut = sdf.parse(sc.next());
 
         if (!checkOut.after(checkIn)) {
@@ -35,19 +36,16 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if(checkIn.before(now) || checkIn.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else if (!checkOut.after(checkIn)) {
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
-            }
-            else {
-                reservation.updateDates(checkIn, checkOut);
-                System.out.println("Reservation: " + reservation);
+
+            String error = reservation.updateDates(checkIn, checkOut);
+               if (error != null) {
+                    System.out.println("Error in reservation: " + error);
+               }
+               else
+                    System.out.println("Reservation: " + reservation);
             }
 
            sc.close();
         }
     }
-}
+
